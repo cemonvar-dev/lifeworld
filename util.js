aa669c0acb092df3c3aab9bf67585751dbbaa39f
cemonvar-dev <cem.onvar@gmail.com>
2025-11-30 22:23:18 +0300

    Add files via upload

--- util.js
+++ util.js
@@ -4,3 +4,10 @@ function addDays(date, days) {
   d.setDate(d.getDate() + days);
   return d;
 }
+
+function formatDate(d) {
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const day = String(d.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+}
