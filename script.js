610d06d153a859c145f94b63cd2f4596a35ac93c
cemonvar-dev <cem.onvar@gmail.com>
2025-11-30 23:12:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -337,7 +337,7 @@ function formatFrequency(freq) {
 }
 
 function calculateNextOccurrence(freq, lastUpdateDate) {
-	const baseDate = lastUpdateDate ? new Date(lastUpdateDate) : new Date();
+	const baseDate = lastUpdateDate ? new Date(convertToShortDate(lastUpdateDate)) : new Date();
 	let mode = freq.mode;
 
 	switch (mode) {
